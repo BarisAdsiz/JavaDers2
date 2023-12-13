01bcb0b897b8ff121699a14a92d2db77316c442f
Barış Adsız <123736772+BarisAdsiz@users.noreply.github.com>
2023-12-13 14:17:58 +0300

    Add files via upload

---
+++ src/ders_2/Ders_2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ders_2;
+
+/**
+ *
+ * @author Barış Adsız
+ */
+public class Ders_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //YORUM SATIRI KULLANIMI
+        
+        //KOD EKRANA YAZI YAZDIRIR
+        System.out.println("Baris Adsiz");
+    }
+    
+}
